af1822c0199d6fdc2e1d597304c8fb77a20579e6
Akshay <akshay.pandey2021@vitbhopal.ac.in>
2024-10-12 19:57:47 +0530

    AWS ECS deployment

---
+++ SimpleWebApp/src/main/java/com/akshay/SimpleWebApp/HelloController.java
@@ -0,0 +1,23 @@
+package com.akshay.SimpleWebApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet() {
+        return "Welcome Home!";
+    }
+
+    @GetMapping("info")
+    public String aboutMachine() {
+        String os = System.getProperty("os.name");
+        String version = System.getProperty("os.version");
+        String architecture = System.getProperty("os.arch");
+        String javaVersion = System.getProperty("java.version");
+        String user = System.getProperty("user.name");
+
+        return "OS: " + os + ", Version: " + version + ", Architecture: " + architecture + ", Java Version: " + javaVersion + ", User: " + user;
+    }
+}
